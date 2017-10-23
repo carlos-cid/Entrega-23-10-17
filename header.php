447ef8b0a7a6b7f16ef1378b82cf836ebe4d8d41
carlos-cid <carloscids.44@gmail.com>
2017-10-23 12:43:41 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -6,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="…">
     <meta name="author" content="…">
-    <title>Bootstrap + PHP + CSV</title>
+    <title>ELecciones 2017</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fix.css">
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="index.php">Avance</a>
+      <a class="navbar-brand" href="index.php">Home</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,11 +19,11 @@
         <ul class="navbar-nav ml-auto">
 
           <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='index.php'){?>active<?php };?>"><a class="nav-link" href="index.php">Presidentes</a></li>
-          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='senadores.php'){?>active<?php };?>"><a class="nav-link" href="senadores-genero.php">Senadores por género</a></li>
-          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='senadores.php'){?>active<?php };?>"><a class="nav-link" href="senadores-pactos.php">Senadores por pacto</a></li>
-          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='diputados.php'){?>active<?php };?>"><a class="nav-link" href="diputados-genero.php">Diputados por género</a></li>
-          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='diputados.php'){?>active<?php };?>"><a class="nav-link" href="diputados-pactos.php">Diputados por pacto</a></li>
-          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='diputados.php'){?>active<?php };?>"><a class="nav-link" href="visualizaciones.php">Visualizaciones</a></li>
+          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='senadores-genero.php'){?>active<?php };?>"><a class="nav-link" href="senadores-genero.php">Senadores por género</a></li>
+          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='senadores-pacto.php'){?>active<?php };?>"><a class="nav-link" href="senadores-pactos.php">Senadores por pacto</a></li>
+          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='diputados-genero.php'){?>active<?php };?>"><a class="nav-link" href="diputados-genero.php">Diputados por género</a></li>
+          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='diputados-pactos.php'){?>active<?php };?>"><a class="nav-link" href="diputados-pactos.php">Diputados por pacto</a></li>
+          <li class="nav-item <?php if((basename($_SERVER['PHP_SELF']))=='visualizaciones.php'){?>active<?php };?>"><a class="nav-link" href="visualizaciones.php">Visualizaciones</a></li>
 
         </ul>
       </div>
@@ -32,18 +31,15 @@
     <?php if((basename($_SERVER['PHP_SELF']))=='index.php'){?>
     <div class="container">
       <div class="jumbotron">
-        <h1>Bootstrap + PHP + CSV</h1>
-        <p class="lead"><a href="http://getbootstrap.com">Bootstrap</a> es un <a href="https://es.wikipedia.org/wiki/Framework">Framework</a> de <a href="https://developer.mozilla.org/es/docs/Glossary/HTML">HTML</a>, <a href="https://developer.mozilla.org/es/docs/Glossary/CSS">CSS</a> y <a href="https://developer.mozilla.org/es/docs/Glossary/JavaScript">JS</a> para implementar <a href="https://es.wikipedia.org/wiki/Dise%C3%B1o_web_adaptable">diseño web adaptable</a>. Optar por Bootstrap es optar por una de las opciones más populares, por asegurarse la disponibilidad de mucha ayuda en línea.</p>
-        <p><a href="http://www.php.net/manual/es/">PHP</a> es un lenguaje de código abierto que es ejecutado en el servidor, generando HTML y enviándolo al cliente. El gran parecido que posee PHP con los lenguajes más comunes de programación estructurada, como C y Perl, permiten a la mayoría de los programadores crear aplicaciones complejas con una curva de aprendizaje muy corta.</p>
-        <p><acronym title="comma-separated values">CSV</acronym> son un tipo de documento en formato abierto sencillo para representar datos en forma de tabla, en las que las columnas se separan por comas y las filas por saltos de línea.</p>
-      </div>
+        <h1>ELecciones 2017</h1>
+          </div>
     </div>
     <?php } else {;?>
     <div class="container">
       <div class="jumbotron">
-        <h1>Bootstrap + PHP + CSV</h1>
-        <p class="lead">Ahora probemos hagamos algunas pruebas.</p>
+        <h1>ELecciones 2017</h1>
       </div>
     </div>
 
  <?php };?>
+
